076f95d8d6447033492828006c46c1665cd08d84
PR0C3S <johnhpena@gmail.com>
2024-02-16 12:34:57 -0400

    Understanding Node JS behind the scenes

---
+++ 2-how-node-works/test-module-1.js
@@ -0,0 +1,31 @@
+// *another way
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   sub(a, b) {
+//     return a - b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
+//module.exports = Calculator;
+
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+  sub(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+};
