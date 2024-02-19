eb3ed638831f40aea132bd3ffc76866849f3422b
PR0C3S <johnhpena@gmail.com>
2024-02-19 16:05:29 -0400

    Serving Static Files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,8 +6,9 @@ const userRouter = require('./routes/userRoutes');
 
 //* 1) MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
