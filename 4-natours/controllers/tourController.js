1906c6828244f93a9b3a47bca72a7b3e21d6032c
PR0C3S <johnhpena@gmail.com>
2024-02-22 16:29:29 -0400

    Adding pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -6,12 +6,11 @@ exports.getAllTours = async (req, res) => {
     //1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|g|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     let query = await Tour.find(JSON.parse(queryStr));
     //2) Sorting
@@ -29,6 +28,19 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+
+    //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    //page=2&limit=10 1-10 page1, 11-20 page2
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
