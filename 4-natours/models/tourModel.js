c28d98a0f3f7e074a645b02e73eb670a5123fada
JOHN  H. PEÑA <50205406+PR0C3S@users.noreply.github.com>
2024-02-25 17:55:29 -0400

    Addin data validation

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -7,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
     slug: String,
     duration: {
@@ -20,8 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
-    ratingAverage: { type: Number, default: 4.5 },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating be above 1.0'],
+      max: [5, 'Rating be below 5.0'],
+    },
     ratingQuantity: { type: Number, defaul: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
     priceDiscount: Number,
@@ -56,6 +68,10 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
@@ -85,8 +101,13 @@ tourSchema.post(/^find/, function (doc, next) {
   next();
 });
 
-tourSchema.virtual('durationWeeks').get(function () {
-  return this.duration / 7;
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  console.log(this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
