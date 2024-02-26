6e1c62b914b042e81cbc8bc969fc3409686326ea
PR0C3S <johnhpena@gmail.com>
2024-02-26 15:50:17 -0400

    Adding 404 not found route

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const { findOneAndDelete } = require('./models/tourModel');
 
 //* 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -13,11 +14,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -25,5 +21,12 @@ app.use((req, res, next) => {
 //* 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: failed,
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
